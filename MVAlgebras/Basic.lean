1f26f868958627400a021b6795cc4d222ba8e41b
MassimoMarruso <massimomarruso13@gmail.com>
2026-04-30 19:40:27 +0200

    Changed definition of MVAlgebra to avoid redundant axioms, deriving the usual definition

--- MVAlgebras/Basic.lean
+++ MVAlgebras/Basic.lean
@@ -2,10 +2,6 @@ import Mathlib.Algebra.Group.Basic
 import Mathlib.Order.Defs.PartialOrder
 import Mathlib.Tactic
 
-/-! This file adds definitions, and proves several simple identities
-
---/
-
 /- An MV Algebra is a is an algebra with
   certain axioms. The operations are here denoted
   + and -, and the constant is 0
@@ -15,7 +11,85 @@ import Mathlib.Tactic
   Since ¬ is already interpreted as negation of Prop, it cannot be used
   -/
 
+/-- We however define an MV-Algebra as an Algebra with fewer axioms, with
+  a binary +, an unary - and a constant 0
+  This ends up being a commutative monoid, but commutativity is not asked as an axiom
+  In fact, being a monoid is not asked at all: the element zero is only required
+  to be a right identity, it being a left identity comes later
+  Notice how two of the axioms are stated in the definition implicity,
+  by saying that neg is involutive and + is a semigroup -/
+
+class MVAlgebra (A : Type*) extends AddSemigroup A, InvolutiveNeg A, Zero A where
+  add_zero (x : A) : x + 0 = x
+  add_neg_zero (x : A) : x + (- (0 : A)) = - (0 : A)
+  neg_switch (x y : A) : - ((- x) + y) + y = (- ((- y) + x)) + x
+
+open MVAlgebra
+
+lemma lem₁ [MVAlgebra A]
+  (x y : A) : x + y = x + (0 + y) := by
+    calc x + y
+    _ = (x + 0) + y := by rw[add_zero x]
+    _ = x + (0 + y) := by rw[add_assoc]
+
+lemma lem₂ [MVAlgebra A]
+  (x : A) : - (- 0 + x) + x = x := by
+      calc - (-0 + x) + x
+      _ = - (- x + 0) + 0 := neg_switch 0 x
+      _ = - - x := by rw[add_zero (- x),add_zero (- - x)]
+      _ = x := by rw[neg_neg]
+
+lemma lem₃ [MVAlgebra A]
+  (x : A) : 0 + x = x := by
+  calc 0 + x
+    _ = - (- 0 + (0 + x)) + (0 + x) := (lem₂ (0 + x)).symm
+    _ = - (- 0 + 0 + x) + 0 + x := by rw[add_assoc,add_assoc]
+    _ = - (- 0 + x) + x := by rw[MVAlgebra.add_zero,MVAlgebra.add_zero]
+    _ = x := lem₂ x
+
+lemma lem₄ [MVAlgebra A]
+  (x y z : A) : - (- x + y) + (y + z) = - (- y + x) + (x + z) := by
+      calc - (- x + y) + (y + z)
+      _ = - (- x + y) + y + z := by rw[add_assoc]
+      _ = - (- y + x) + x + z := by rw[neg_switch]
+      _ = - (- y + x) + (x + z) := by rw[add_assoc]
+
+lemma lem₅ [MVAlgebra A]
+  (x : A) : - 0 = - x + x := by
+    calc - 0
+    _ = - (- x + - 0) + - 0 := (add_neg_zero (- (- x + - 0))).symm
+    _ = - (- - 0 + x) + x := by rw[neg_switch]
+    _ = - (0 + x) + x := by rw[neg_neg]
+    _ = - x + x := by rw[lem₃ x]
+
+lemma lem₆ [MVAlgebra A]
+  (x y : A) : - 0 = - (y + x) + (x + y) := by
+    calc - 0
+    _ = - (- x + - y) + - 0 := by rw[add_neg_zero (- (-x + - y))]
+    _ = - (- x + - y) + (- y + y) := by rw[lem₅ y]
+    _ = - (- x + - y) + - y + y := by rw[add_assoc]
+    _ = - (- - y + x) + x + y := by rw[neg_switch x (-y)]
+    _ = - (y + x) + (x + y) := by rw[add_assoc,neg_neg]
+
 namespace Basic
+
+instance (A : Type*) [MVAlgebra A] : AddCommMonoid A where
+  add_zero := MVAlgebra.add_zero
+  add_comm := by
+    intro x y
+    calc x + y
+    _ = 0 + (x + y) := by rw[lem₃ (x + y)]
+    _ = - - 0 + (x + y) := by rw[neg_neg]
+    _ = - (- (y + x) + (x + y)) + (x + y) := by rw[lem₆ x y]
+    _ = - (- (x + y) + (y + x)) + (y + x) := (neg_switch (x + y) (y + x)).symm
+    _ = - - 0 + (y + x) := by rw[lem₆ y x]
+    _ = 0 + (y + x) := by rw[neg_neg]
+    _ = y + x := by rw[lem₃ (y + x)]
+  zero_add := lem₃
+  nsmul := nsmulRec
+  nsmul_zero := by tauto
+
+-- now we begin to add the usual notation of 1, ⊙ and ⊖, along with implementing simp
 
 class OTimes (A : Type*) where
   oTimes : A → A → A
@@ -25,10 +99,6 @@ class ONeg (A : Type*) where
 
 infix:500 "⊙" => OTimes.oTimes
 infix:600 "⊖" => ONeg.oNeg
-
-class MVAlgebra (A : Type*) extends AddCommMonoid A, InvolutiveNeg A where
-  add_neg_zero {x : A} : x + (- (0 : A)) = - (0 : A)
-  neg_switch {x y : A} : - ((- x) + y) + y = (- ((- y) + x)) + x
 
 instance [MVAlgebra A] : One A where
   one := - (0 : A)
@@ -45,7 +115,7 @@ theorem oTimes_neg_add [MVAlgebra A] (x y : A) : x ⊙ y = - (-x + -y) := rfl
 theorem oNeg_def [MVAlgebra A] (x y : A) : x ⊖ y = x ⊙ (-y) := rfl
 @[simp]
 theorem neg_switch [MVAlgebra A] (x y : A) : - (- x + y) + y = - (- y + x) + x :=
-  MVAlgebra.neg_switch
+  MVAlgebra.neg_switch _ _
 
 @[simp]
 lemma oNeg_def' [MVAlgebra A] (x y : A) : x ⊖ y = - (- x + y) := by
@@ -60,7 +130,7 @@ theorem neg_one [MVAlgebra A] : - (1 : A) = 0 := by
 @[simp]
 theorem add_one [MVAlgebra A] (x : A) : x + 1 = 1 := by
   rw[neg_zero]
-  exact MVAlgebra.add_neg_zero
+  exact MVAlgebra.add_neg_zero _
 
 @[simp]
 lemma one_add [MVAlgebra A] (x : A) : 1 + x = 1 := by
