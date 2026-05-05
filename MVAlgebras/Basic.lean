ab5b08d3cb177faddcb27d28f20d309692ca87ae
MassimoMarruso <massimomarruso13@gmail.com>
2026-05-05 19:04:30 +0200

    Changed notation to be more like the usual ones. Added a few lemmas

--- MVAlgebras/Basic.lean
+++ MVAlgebras/Basic.lean
@@ -4,9 +4,9 @@ import Mathlib.Tactic
 
 /- An MV Algebra is a commutative monoid with an involution ¬
   with the following further axioms
-  x + ¬ 0 = ¬ 0 and
-  ¬ (¬ x + y) + y = ¬ (¬y + x) + x
-  The usual notation for the operation here called + is ⊕
+  x ⊕ ¬ 0 = ¬ 0 and
+  ¬ (¬ x ⊕ y) ⊕ y = ¬ (¬y ⊕ x) ⊕ x
+  The usual notation for the operation here called ⊕ is ⊕
   and we also change the notation from ¬ to - (it's a minus sign)
   This allows the code to see it as an AddCommMonoid and InvolutiveNeg
   with the two axioms above
@@ -18,69 +18,99 @@ import Mathlib.Tactic
   a right identity
   We follow Kolařík's proof exactly, defining MV-Algebras with the reduced set of axioms
   and then proving that any MVAlgebra is an AddCommMonoid-/
-class MVAlgebra (A : Type*) extends AddSemigroup A, InvolutiveNeg A, Zero A where
-  add_zero (x : A) : x + 0 = x
-  add_neg_zero (x : A) : x + (- (0 : A)) = - (0 : A)
-  neg_switch (x y : A) : - ((- x) + y) + y = (- ((- y) + x)) + x
 
-open MVAlgebra
+namespace Basic
+
+class OAdd (A : Type*) where
+  oAdd : A → A → A
+
+class Not (A : Type*) where
+  not : A → A
+
+--prefix:max (priority := default) "-"  => Not.not
+infixl:74 (priority := default) " ⊕ "   => OAdd.oAdd
+
+variable (A : Type*) [OAdd A] [Neg A] (x y z : A)
+
+class MVAlgebra (A : Type*) extends OAdd A, InvolutiveNeg A, Zero A where
+  oAdd_assoc (x y z : A) : ((x ⊕ y) ⊕ z) = x ⊕ (y ⊕ z)
+  oAdd_zero (x : A) : (x ⊕ 0) = x
+  oAdd_not_zero (x : A) : (x ⊕ (- (0 : A))) = - (0 : A)
+  not_switch (x y : A) : ((- ((- x) ⊕ y)) ⊕ y) = (- ((- y) ⊕ x)) ⊕ x
 
 variable {A : Type*} [MVAlgebra A]
 
-lemma lem₁ (x y : A) : x + y = x + (0 + y) := by
-    calc x + y
-    _ = (x + 0) + y := by rw[add_zero x]
-    _ = x + (0 + y) := by rw[add_assoc]
+@[simp]
+lemma oAdd_assoc (x y z : A) : ((x ⊕ y) ⊕ z) = x ⊕ (y ⊕ z) := MVAlgebra.oAdd_assoc _ _ _
 
-lemma lem₂ (x : A) : - (- 0 + x) + x = x := by
-      calc - (-0 + x) + x
-      _ = - (- x + 0) + 0 := neg_switch 0 x
-      _ = - - x := by rw[add_zero (- x),add_zero (- - x)]
-      _ = x := by rw[neg_neg]
+@[simp]
+lemma oAdd_zero (x : A) : (x ⊕ 0) = x := MVAlgebra.oAdd_zero _
 
-lemma lem₃ (x : A) : 0 + x = x := by
-  calc 0 + x
-    _ = - (- 0 + (0 + x)) + (0 + x) := (lem₂ (0 + x)).symm
-    _ = - (- 0 + 0 + x) + 0 + x := by rw[add_assoc,add_assoc]
-    _ = - (- 0 + x) + x := by rw[MVAlgebra.add_zero,MVAlgebra.add_zero]
+@[simp]
+lemma oAdd_not_zero (x : A) : (x ⊕ (- (0 : A))) = - (0 : A) :=
+  MVAlgebra.oAdd_not_zero _
+
+@[simp]
+lemma not_switch (x y : A) : ((- ((- x) ⊕ y)) ⊕ y) = (- ((- y) ⊕ x)) ⊕ x :=
+  MVAlgebra.not_switch _ _
+
+lemma lem₁ (x y : A) : (x ⊕ y) = x ⊕ (0 ⊕ y) := by
+    calc x ⊕ y
+    _ = (x ⊕ 0) ⊕ y := by rw[oAdd_zero x]
+    _ = x ⊕ (0 ⊕ y) := by rw[oAdd_assoc]
+
+lemma lem₂ (x : A) : ((- ((- 0 : A) ⊕ x)) ⊕ x) = x := by
+      calc (- ((- 0 : A) ⊕ x)) ⊕ x
+      _ = (- (-x ⊕ 0)) ⊕ 0 := by rw[not_switch]
+      _ = - - x := by rw[oAdd_zero (- x),oAdd_zero (- - x)]
+      _ = x := by rw[neg_neg x]
+
+lemma zero_oAdd (x : A) : (0 ⊕ x) = x := by
+  calc 0 ⊕ x
+    _ = (- (- 0 ⊕ (0 ⊕ x))) ⊕ (0 ⊕ x) := (lem₂ (0 ⊕ x)).symm
+    _ = ((- ((- 0 ⊕ 0) ⊕ x)) ⊕ 0) ⊕ x := by rw[oAdd_assoc,oAdd_assoc]
+    _ = (- (- 0 ⊕ x)) ⊕ x := by rw[MVAlgebra.oAdd_zero,MVAlgebra.oAdd_zero]
     _ = x := lem₂ x
 
-lemma lem₄ (x y z : A) : - (- x + y) + (y + z) = - (- y + x) + (x + z) := by
-      calc - (- x + y) + (y + z)
-      _ = - (- x + y) + y + z := by rw[add_assoc]
-      _ = - (- y + x) + x + z := by rw[neg_switch]
-      _ = - (- y + x) + (x + z) := by rw[add_assoc]
+lemma lem₄ (x y z : A) : ((- (- x ⊕ y)) ⊕ (y ⊕ z)) = (- (- y ⊕ x)) ⊕ (x ⊕ z) := by
+      calc (- (- x ⊕ y)) ⊕ (y ⊕ z)
+      _ = ((- (- x ⊕ y)) ⊕ y) ⊕ z := by rw[oAdd_assoc]
+      _ = ((- (- y ⊕ x)) ⊕ x) ⊕ z := by rw[not_switch]
+      _ = (- (- y ⊕ x)) ⊕ (x ⊕ z) := by rw[oAdd_assoc]
 
-lemma lem₅ (x : A) : - 0 = - x + x := by
+lemma lem₅ (x : A) : - 0 = - x ⊕ x := by
     calc - 0
-    _ = - (- x + - 0) + - 0 := (add_neg_zero (- (- x + - 0))).symm
-    _ = - (- - 0 + x) + x := by rw[neg_switch]
-    _ = - (0 + x) + x := by rw[neg_neg]
-    _ = - x + x := by rw[lem₃ x]
+    _ = - (- x ⊕ - 0) ⊕ - 0 := (oAdd_not_zero (- (- x ⊕ - 0))).symm
+    _ = - (- - 0 ⊕ x) ⊕ x := by rw[not_switch]
+    _ = - (0 ⊕ x) ⊕ x := by rw[neg_neg]
+    _ = - x ⊕ x := by rw[zero_oAdd x]
 
-lemma lem₆ (x y : A) : - 0 = - (y + x) + (x + y) := by
+lemma lem₆ (x y : A) : - 0 = - (y ⊕ x) ⊕ (x ⊕ y) := by
     calc - 0
-    _ = - (- x + - y) + - 0 := by rw[add_neg_zero (- (-x + - y))]
-    _ = - (- x + - y) + (- y + y) := by rw[lem₅ y]
-    _ = - (- x + - y) + - y + y := by rw[add_assoc]
-    _ = - (- - y + x) + x + y := by rw[neg_switch x (-y)]
-    _ = - (y + x) + (x + y) := by rw[add_assoc,neg_neg]
+    _ = - (- x ⊕ - y) ⊕ - 0 := by rw[oAdd_not_zero (- (-x ⊕ - y))]
+    _ = - (- x ⊕ - y) ⊕ (- y ⊕ y) := by rw[lem₅ y]
+    _ = - (- x ⊕ - y) ⊕ - y ⊕ y := by rw[oAdd_assoc]
+    _ = - (- - y ⊕ x) ⊕ x ⊕ y := by rw[not_switch x (-y)]
+    _ = - (y ⊕ x) ⊕ (x ⊕ y) := by rw[oAdd_assoc,neg_neg]
 
-instance : AddCommMonoid A where
-  add_zero := MVAlgebra.add_zero
-  add_comm := by
-    intro x y
-    calc x + y
-    _ = 0 + (x + y) := by rw[lem₃ (x + y)]
-    _ = - - 0 + (x + y) := by rw[neg_neg]
-    _ = - (- (y + x) + (x + y)) + (x + y) := by rw[lem₆ x y]
-    _ = - (- (x + y) + (y + x)) + (y + x) := (neg_switch (x + y) (y + x)).symm
-    _ = - - 0 + (y + x) := by rw[lem₆ y x]
-    _ = 0 + (y + x) := by rw[neg_neg]
-    _ = y + x := by rw[lem₃ (y + x)]
-  zero_add := lem₃
-  nsmul := nsmulRec
-  nsmul_zero := by tauto
+@[simp]
+theorem oAdd_comm (x y : A) : (x ⊕ y) = y ⊕ x := by
+  calc x ⊕ y
+  _ = 0 ⊕ (x ⊕ y) := by rw[zero_oAdd (x ⊕ y)]
+  _ = - - 0 ⊕ (x ⊕ y) := by rw[neg_neg]
+  _ = - (- (y ⊕ x) ⊕ (x ⊕ y)) ⊕ (x ⊕ y) := by rw[lem₆ x y]
+  _ = - (- (x ⊕ y) ⊕ (y ⊕ x)) ⊕ (y ⊕ x) := (not_switch (x ⊕ y) (y ⊕ x)).symm
+  _ = - - 0 ⊕ (y ⊕ x) := by rw[lem₆ y x]
+  _ = 0 ⊕ (y ⊕ x) := by rw[neg_neg]
+  _ = y ⊕ x := by rw[zero_oAdd (y ⊕ x)]
+
+instance : CommMonoid A where
+  mul := OAdd.oAdd
+  mul_assoc := oAdd_assoc
+  one := 0
+  mul_one := MVAlgebra.oAdd_zero
+  mul_comm := oAdd_comm
+  one_mul := zero_oAdd
 
 -- we now begin to add the usual notation of 1, ⊙ and ⊖, along with implementing simp
 
@@ -97,84 +127,84 @@ instance : One A where
   one := - (0 : A)
 
 instance : OTimes A where
-  oTimes x y := - ((- x) + - y)
+  oTimes x y := - ((- x) ⊕ - y)
 
 instance : ONeg A where
   oNeg x y := x ⊙ (- y)
 
 @[simp]
-lemma neg_zero' : (1 : A) = - (0 : A) := rfl
-lemma oTimes_dual (x y : A) : x ⊙ y = - (-x + -y) := rfl
+lemma not_zero : (1 : A) = - (0 : A) := rfl
+lemma oTimes_dual (x y : A) : x ⊙ y = - (-x ⊕ -y) := rfl
 lemma oNeg_def (x y : A) : x ⊖ y = x ⊙ (-y) := rfl
 
 @[simp]
-lemma oNeg_def' (x y : A) : x ⊖ y = - (- x + y) := by
+lemma oNeg_def' (x y : A) : x ⊖ y = - (- x ⊕ y) := by
   rw[oNeg_def,oTimes_dual,neg_neg]
 
 @[simp]
-lemma neg_one : - (1 : A) = 0 := by
+lemma not_one : - (1 : A) = 0 := by
   calc - (1 : A)
-  _ = - - (0 : A) := by rw[neg_zero']
+  _ = - - (0 : A) := by rw[not_zero]
   _ = 0 := by rw[neg_neg]
 
 @[simp]
-lemma add_one (x : A) : x + 1 = 1 := by
-  rw[neg_zero']
-  exact MVAlgebra.add_neg_zero _
+lemma oAdd_one (x : A) : (x ⊕ 1) = 1 := by
+  rw[not_zero]
+  exact MVAlgebra.oAdd_not_zero _
 
 @[simp]
-lemma one_add (x : A) : 1 + x = 1 := by
-  rw[add_comm]
-  exact add_one x
+lemma one_oAdd (x : A) : (1 ⊕ x) = 1 := by
+  rw[oAdd_comm]
+  exact oAdd_one x
 
-lemma add_dual {x y : A} : x + y = - ((- x) ⊙ (- y)) := by
-  calc x + y
-  _ = - - (x + y) := by rw[neg_neg]
-  _ = - - (- - x + - - y) := by rw[neg_neg x,neg_neg y]
+lemma oAdd_dual {x y : A} : (x ⊕ y) = - ((- x) ⊙ (- y)) := by
+  calc x ⊕ y
+  _ = - - (x ⊕ y) := by rw[neg_neg]
+  _ = - - (- - x ⊕ - - y) := by rw[neg_neg x,neg_neg y]
   _ = - ((- x) ⊙ (- y)) := by rw[oTimes_dual]
 
 @[simp]
-lemma add_canc' (x : A) : x + - x = 1 := by
-  calc x + - x
-  _ = - x + x := by rw[add_comm]
-  _ = - (0 + x) + x := by rw[zero_add]
-  _ = - (- (1 : A) + x) + x := by rw[neg_one]
-  _ = - (- x + 1) + 1 := by rw[neg_switch]
-  _ = 1 := by rw[add_one]
+lemma oAdd_canc (x : A) : (x ⊕ - x) = 1 := by
+  calc x ⊕ - x
+  _ = - x ⊕ x := by rw[oAdd_comm]
+  _ = - (0 ⊕ x) ⊕ x := by rw[zero_oAdd]
+  _ = - (- (1 : A) ⊕ x) ⊕ x := by rw[not_one]
+  _ = - (- x ⊕ 1) ⊕ 1 := by rw[not_switch]
+  _ = 1 := by rw[oAdd_one]
 
 @[simp]
-lemma add_canc (x : A) : - x + x = 1 := by
-  rw[add_comm]
-  exact add_canc' x
+lemma oAdd_canc' (x : A) : (- x ⊕ x) = 1 := by
+  rw[oAdd_comm]
+  exact oAdd_canc x
 
 @[simp]
-lemma add_dual' {x y : A} : - (x + y) = (- x) ⊙ (- y) := by
-  calc - (x + y)
-  _ = - (- - x + - - y) := by rw[neg_neg,neg_neg]
+lemma oAdd_dual' {x y : A} : - (x ⊕ y) = (- x) ⊙ (- y) := by
+  calc - (x ⊕ y)
+  _ = - (- - x ⊕ - - y) := by rw[neg_neg,neg_neg]
   _ = (- x) ⊙ (- y) := by rw[oTimes_dual]
 
 @[simp]
-lemma oTimes_dual' {x y : A} : - (x ⊙ y) = - x + - y := by
+lemma oTimes_dual' {x y : A} : - (x ⊙ y) = - x ⊕ - y := by
   calc - (x ⊙ y)
   _ = - ((- - x) ⊙ (- - y)) := by rw[neg_neg,neg_neg]
-  _ = - x + - y := by rw[add_dual]
+  _ = - x ⊕ - y := by rw[oAdd_dual]
 
-lemma minus_add {x y : A} : (x ⊖ y) + y = (y ⊖ x) + x := by
-  calc (x ⊖ y) + y
-  _ = - (- x + y) + y := by simp
-  _ = - (- y + x) + x := by rw[neg_switch]
-  _ = (y ⊖ x) + x := by simp
+lemma oNeg_oAdd {x y : A} : ((x ⊖ y) ⊕ y) = (y ⊖ x) ⊕ x := by
+  calc (x ⊖ y) ⊕ y
+  _ = - (- x ⊕ y) ⊕ y := by simp
+  _ = - (- y ⊕ x) ⊕ x := by rw[not_switch]
+  _ = (y ⊖ x) ⊕ x := by simp
 
 @[simp]
 lemma oTimes_comm (x y : A) : x ⊙ y = y ⊙ x := by
-  rw[oTimes_dual,oTimes_dual,add_comm]
+  rw[oTimes_dual,oTimes_dual,oAdd_comm]
 
 @[simp]
 lemma oTimes_canc (x : A) : x ⊙ (- x) = 0 := by
   calc x ⊙ (- x)
-  _ = - (- x + x) := by rw[oTimes_dual,neg_neg]
-  _ = - 1 := by rw[add_canc]
-  _ = 0 := by rw[neg_one]
+  _ = - (- x ⊕ x) := by rw[oTimes_dual,neg_neg]
+  _ = - 1 := by rw[oAdd_canc']
+  _ = 0 := by rw[not_one]
 
 @[simp]
 lemma oTimes_canc' (x : A) : (- x) ⊙ x = 0 := by
@@ -182,7 +212,7 @@ lemma oTimes_canc' (x : A) : (- x) ⊙ x = 0 := by
   rw[oTimes_canc]
 
 @[simp]
-lemma neg_iff_neg (x y : A) : x = y ↔ - x = - y := by
+lemma not_iff_not' (x y : A) : x = y ↔ - x = - y := by
   apply Iff.intro
   case mp =>
     intro h
@@ -196,11 +226,19 @@ lemma neg_iff_neg (x y : A) : x = y ↔ - x = - y := by
     _ = y := by rw[neg_neg]
 
 @[simp]
-lemma neg_switch (x y : A) :
-  - ((- x) + y) + y = (- ((- y) + x)) + x := MVAlgebra.neg_switch _ _
+lemma oTimes_assoc (x y z : A) : x ⊙ (y ⊙ z) = (x ⊙ y) ⊙ z := by
+  rw[not_iff_not']
+  calc - x ⊙ (y ⊙ z)
+  _ = (- x) ⊕ - (y ⊙ z) := by rw[oTimes_dual']
+  _ = (- x) ⊕ ((- y) ⊕ (- z)) := by rw[oTimes_dual']
+  _ = ((- x) ⊕ (- y)) ⊕ (- z) := by rw[oAdd_assoc]
+  _ = (- (x ⊙ y)) ⊕ (- z) := by rw[oTimes_dual']
+  _ = - ((x ⊙ y) ⊙ z) := by rw[←oTimes_dual']
 
 @[simp]
-lemma neg_switch' (x y : A) :
-  ( x ⊖ y) + y = ( y ⊖ x) + x := by
+lemma not_switch' (x y : A) :
+  ((x ⊖ y) ⊕ y) = ( y ⊖ x) ⊕ x := by
   rw[oNeg_def',oNeg_def']
-  apply _root_.neg_switch
+  apply not_switch
+
+end Basic
--- MVAlgebras/NaturalOrder.lean
+++ MVAlgebras/NaturalOrder.lean
@@ -1,48 +1,55 @@
 import MVAlgebras.Basic
 
+open Basic
 /- This file introduces what is called the "Natural Order" on MVAlgebras
   The beginning of the file introduces a notation ≤ without any propreties, then
   equivalent notions are derived. Finally, it is proven to be a partial order
-  This proof is from the first chapter of Mundici -/
+  This proof is from the first chapter of Mundici
+  Later in the file it is proven to be a lattice-/
 
 variable {A : Type*} [MVAlgebra A]
 
-instance : LE A where
-  le x y := (- x) + y = 1
+instance [MVAlgebra A] : LE A where
+  le x y := ((- x) ⊕ y) = 1
 
-lemma le_iff₁ {x y : A} : x ≤ y ↔ (- x) + y = 1 := by
+lemma le_iff₁ {x y : A} : x ≤ y ↔ ((- x) ⊕ y) = 1 := by
   tauto
 
 lemma le_iff₂ {x y : A} : x ≤ y ↔ x ⊙ (- y) = 0 := by
   calc x ≤ y
-  _ ↔ (- x) + y = 1 := by rw[le_iff₁]
+  _ ↔ ((- x) ⊕ y) = 1 := by rw[le_iff₁]
   _ ↔ - (x ⊙ (- y)) = 1 := by rw[oTimes_dual',neg_neg y]
-  _ ↔ - (x ⊙ (- y)) = - 0 := by rw[neg_zero']
-  _ ↔ x ⊙ (-y) = 0 := by rw[←neg_iff_neg]
+  _ ↔ - (x ⊙ (- y)) = - 0 := by rw[not_zero]
+  _ ↔ (x ⊙ (-y)) = 0 := by rw[←not_iff_not' _ 0]
 
-lemma le_iff₃ {x y : A} : x ≤ y ↔ y = x + (y ⊖ x) := by
+lemma le_iff₃ {x y : A} : x ≤ y ↔ y = (x ⊕ (y ⊖ x)) := by
   apply Iff.intro
   case mp =>
     intro h
     calc y
-    _ = (x ⊙ (- y)) + y := by simp[le_iff₂.mp h]
-    _ = - (- x + y) + y := by simp
-    _ = - (- y + x) + x := by rw[neg_switch]
-    _ = x + - (- y + x) := by rw[add_comm]
-    _ = x + (y ⊖ x) := by simp
+    _ = 0 ⊕ y := by rw[zero_oAdd]
+    _ = ((x ⊙ (- y)) ⊕ y) := by rw[le_iff₂.mp h]
+    _ = - (- x ⊕ y) ⊕ y := by simp
+    _ = - (- y ⊕ x) ⊕ x := by rw[not_switch]
+    _ = x ⊕ - (- y ⊕ x) := by rw[oAdd_comm]
+    _ = x ⊕ (y ⊖ x) := by simp
   case mpr =>
     intro h
     apply le_iff₂.mpr
     calc x ⊙ (- y)
-    _ = - (- x + y) := by simp
-    _ = - (- x + (x + y ⊖ x)) := by rw[←h]
-    _ = - (- x + (y + x ⊖ y)) := by rw[add_comm y,minus_add,add_comm x]
-    _ = - ((- x + y) + x ⊖ y) := by rw[add_assoc]
-    _ = - (- (x ⊖ y) + x ⊖ y) := by simp
-    _ = - (1 : A) := by rw[add_canc (x ⊖ y)]
+    _ = - (- x ⊕ y) := by simp
+    _ = - (- x ⊕ (x ⊕ y ⊖ x)) := by rw[←h]
+    _ = - (- x ⊕ (y ⊖ x ⊕ x)) := by rw[oAdd_comm x]
+    _ = - ((y ⊖ x ⊕ x) ⊕ - x) := by rw[oAdd_comm (-x)]
+    _ = - (((x ⊖ y) ⊕ y) ⊕ - x) := by rw[oNeg_oAdd]
+    _ = - ((x ⊖ y) ⊕ (y ⊕ - x)) := by rw[oAdd_assoc]
+    _ = - ((x ⊖ y) ⊕ (- x ⊕ y)) := by rw[oAdd_comm (- x)]
+    _ = - ((x ⊖ y) ⊕ - - (- x ⊕ y)) := by rw[neg_neg]
+    _ = - ((x ⊖ y) ⊕ - (x ⊖ y)) := by rw[oNeg_def']
+    _ = - (1 : A) := by rw[oAdd_canc (x ⊖ y)]
     _ = 0 := by simp
 
-lemma le_iff₄ {x y : A} : x ≤ y ↔ ∃ (z : A), x + z = y := by
+lemma le_iff₄ {x y : A} : x ≤ y ↔ ∃ (z : A), (x ⊕ z) = y := by
   apply Iff.intro
   case mp =>
     intro h
@@ -51,11 +58,11 @@ lemma le_iff₄ {x y : A} : x ≤ y ↔ ∃ (z : A), x + z = y := by
   case mpr =>
     intro ⟨z,h⟩
     apply le_iff₁.mpr
-    calc -x + y
-    _ = - x + (x + z) := by rw[←h]
-    _ = (- x + x) + z := by rw[add_assoc]
-    _ = 1 + z := by simp
-    _ = 1 := by rw[one_add]
+    calc -x ⊕ y
+    _ = - x ⊕ (x ⊕ z) := by rw[←h]
+    _ = (- x ⊕ x) ⊕ z := by rw[oAdd_assoc]
+    _ = 1 ⊕ z := by simp
+    _ = 1 := by rw[one_oAdd]
 
 instance {A : Type*} [MVAlgebra A] : PartialOrder A where
   le x y := x ≤ y
@@ -67,25 +74,25 @@ instance {A : Type*} [MVAlgebra A] : PartialOrder A where
     intro x y z
     rw[le_iff₄,le_iff₄,le_iff₄]
     intro ⟨x',hx⟩ ⟨y',hy⟩
-    use x' + y'
-    calc x + (x' + y')
-    _ = x + x' + y' := by rw[add_assoc]
-    _ = y + y' := by rw[hx]
+    use x' ⊕ y'
+    calc x ⊕ (x' ⊕ y')
+    _ = x ⊕ x' ⊕ y' := by rw[oAdd_assoc]
+    _ = y ⊕ y' := by rw[hx]
     _ = z := by rw[hy]
   le_antisymm := by
     intro x y h₁ h₂
     replace h₁ : x ⊙ (- y) = 0 := le_iff₂.mp h₁
-    replace h₂ : x = y + (x ⊖ y) := le_iff₃.mp h₂
+    replace h₂ : x = y ⊕ (x ⊖ y) := le_iff₃.mp h₂
     calc x
-    _ = y + (x ⊖ y) := h₂
-    _ = y + (x ⊙ (- y)) := by rw[oNeg_def]
-    _ = y := by rw[h₁,add_zero]
+    _ = y ⊕ (x ⊖ y) := h₂
+    _ = y ⊕ (x ⊙ (- y)) := by rw[oNeg_def]
+    _ = y := by rw[h₁,oAdd_zero]
 
-lemma neg_iff (a : A) (x : A) : a + x = 1 ∧ a ⊙ x = 0 ↔ -a = x := by
+lemma not_iff' (a : A) (x : A) : (a ⊕ x) = 1 ∧ a ⊙ x = 0 ↔ -a = x := by
   apply Iff.intro
   case mp =>
     intro ⟨h₁,h₂⟩
-    replace h₁ : - - a + x = 1 := by
+    replace h₁ : (- - a ⊕ x) = 1 := by
       rw[neg_neg]
       exact h₁
     replace h₁ : -a ≤ x := le_iff₁.mpr h₁
@@ -98,87 +105,153 @@ lemma neg_iff (a : A) (x : A) : a + x = 1 ∧ a ⊙ x = 0 ↔ -a = x := by
   case mpr =>
     intro h
     subst_eqs
-    exact ⟨add_canc' a,oTimes_canc a⟩
+    exact ⟨oAdd_canc a,oTimes_canc a⟩
 
-theorem neg_le' (x y : A) : x ≤ y ↔ - y ≤ - x := by
+theorem not_le' (x y : A) : x ≤ y ↔ - y ≤ - x := by
   calc x ≤ y
-  _ ↔ -x + y = 1 := by rw[le_iff₁]
-  _ ↔ - (x ⊙ (- y)) = 1 := by rw[add_dual,neg_neg]
-  _ ↔ x ⊙ (- y) = 0 := by rw[neg_eq_iff_eq_neg,neg_one]
+  _ ↔ (-x ⊕ y) = 1 := by rw[le_iff₁]
+  _ ↔ - (x ⊙ (- y)) = 1 := by rw[oAdd_dual,neg_neg]
+  _ ↔ (x ⊙ (- y)) = - 1 := by rw[not_iff_not',neg_neg]
+  _ ↔ x ⊙ (- y) = 0 := by rw[not_one]
   _ ↔ (- y) ⊙ (- - x) = 0 := by rw[oTimes_comm,neg_neg]
   _ ↔ - y ≤ - x := by rw[le_iff₂]
 
-theorem add_le' (x y z : A) (h : x ≤ y) : x + z ≤ y + z := by
+theorem oAdd_le (x y z : A) (h : x ≤ y) : (x ⊕ z) ≤ y ⊕ z := by
   apply le_iff₄.mpr
   replace ⟨w,h⟩ := le_iff₄.mp h
   use w
-  calc x + z + w
-  _ = (x + w) + z := by rw[add_right_comm]
-  _ = y + z := by rw[h]
+  calc ((x ⊕ z) ⊕ w)
+  _ = x ⊕ (z ⊕ w) := by rw[oAdd_assoc]
+  _ = x ⊕ (w ⊕ z) := by rw[oAdd_comm z]
+  _ = (x ⊕ w) ⊕ z := by rw[oAdd_assoc]
+  _ = y ⊕ z := by rw[h]
 
 theorem oTimes_le (x y z : A) (h : x ≤ y) : x ⊙ z ≤ y ⊙ z := by
-  suffices this : - (y ⊙ z) ≤ - (x ⊙ z) from by apply (neg_le' _ _).mpr ; exact this
-  suffices this : - y + - z ≤ - x + - z from by rw[oTimes_dual',oTimes_dual'] ; exact this
-  suffices this : -y ≤ - x from add_le' _ _ _ this
-  exact (neg_le' _ _).mp h
+  suffices this : - (y ⊙ z) ≤ - (x ⊙ z) from by apply (not_le' _ _).mpr ; exact this
+  suffices this : (- y ⊕ - z) ≤ - x ⊕ - z from by rw[oTimes_dual',oTimes_dual'] ; exact this
+  suffices this : -y ≤ - x from oAdd_le _ _ _ this
+  exact (not_le' _ _).mp h
 
-theorem sup_le' (x y z : A) (hx : x ≤ z) (hy : y ≤ z) : (x ⊖ y) + y ≤ z := by
+theorem le_oTimes (x y z : A) (h : x ≤ y) : z ⊙ x ≤ z ⊙ y := by
+  nth_rw 1 [oTimes_comm]
+  nth_rw 2 [oTimes_comm]
+  exact oTimes_le _ _ _ h
+
+theorem sup_le' (x y z : A) (hx : x ≤ z) (hy : y ≤ z) : ((x ⊖ y) ⊕ y) ≤ z := by
   replace hx := le_iff₁.mp hx
   replace hy := le_iff₃.mp hy
   apply le_iff₁.mpr
-  calc -(x ⊖ y + y) + z
-    _ = -((- - (x ⊖ y)) + y) + z := by rw[neg_neg]
-    _ = ((- (x ⊖ y)) ⊖ y) + z := by rw[oNeg_def' (- (x ⊖ y))]
-    _ = ((- (x ⊖ y)) ⊖ y) + (y + (z ⊖ y)) := by nth_rewrite 1 [hy] ; tauto
-    _ = ((- (x ⊖ y)) ⊖ y) + y + (z ⊖ y) := by rw[add_assoc]
-    _ = (y ⊖ (- (x ⊖ y))) + (- (x ⊖ y)) + (z ⊖ y) := by rw[neg_switch']
-    _ = (y ⊖ (- (x ⊖ y))) + (- x + y) + (z ⊖ y) := by nth_rewrite 3 [oNeg_def'] ; rw[neg_neg]
-    _ = (y ⊖ (- (x ⊖ y))) + (- x) + (y + (z ⊖ y)) := by rw[add_assoc,add_assoc,add_assoc]
-    _ = (y ⊖ (- (x ⊖ y))) + (- x) + z := by nth_rewrite 1 [←hy] ; tauto
-    _ = (y ⊖ (- (x ⊖ y))) + 1 := by rw[add_assoc,hx]
-    _ = 1 := by rw[add_one]
+  calc -(x ⊖ y ⊕ y) ⊕ z
+    _ = -((- - (x ⊖ y)) ⊕ y) ⊕ z := by rw[neg_neg]
+    _ = ((- (x ⊖ y)) ⊖ y) ⊕ z := by rw[oNeg_def' (- (x ⊖ y))]
+    _ = ((- (x ⊖ y)) ⊖ y) ⊕ (y ⊕ (z ⊖ y)) := by nth_rewrite 1 [hy] ; tauto
+    _ = ((- (x ⊖ y)) ⊖ y) ⊕ y ⊕ (z ⊖ y) := by rw[oAdd_assoc]
+    _ = (y ⊖ (- (x ⊖ y))) ⊕ (- (x ⊖ y)) ⊕ (z ⊖ y) := by rw[not_switch']
+    _ = (y ⊖ (- (x ⊖ y))) ⊕ (- x ⊕ y) ⊕ (z ⊖ y) := by nth_rewrite 3 [oNeg_def'] ; rw[neg_neg]
+    _ = (y ⊖ (- (x ⊖ y))) ⊕ (- x) ⊕ (y ⊕ (z ⊖ y)) := by rw[oAdd_assoc,oAdd_assoc,oAdd_assoc]
+    _ = (y ⊖ (- (x ⊖ y))) ⊕ (- x) ⊕ z := by nth_rewrite 1 [←hy] ; tauto
+    _ = (y ⊖ (- (x ⊖ y))) ⊕ 1 := by rw[oAdd_assoc,hx]
+    _ = 1 := by rw[oAdd_one]
 
-lemma neg_sup' (y z : A) : (-y) ⊖ (-z) + (- z) = -(y ⊙ (-y + z)) := by
-    calc (-y) ⊖ (-z) + (- z)
-    _ = (-z) ⊖ (-y) + (-y) := by rw[neg_switch']
-    _ = (-z) ⊙ (- - y) + (- y) := by rw[oNeg_def]
-    _ = - (z + - y) + -y := by rw[oTimes_dual,neg_neg,neg_neg]
-    _ = -y + - (- y + z) := by rw[add_comm z,add_comm]
-    _ = -(y ⊙ (-y + z)) := by rw[oTimes_dual,neg_neg]
+lemma not_sup' (y z : A) : ((-y) ⊖ (-z) ⊕ (- z)) = -(y ⊙ (-y ⊕ z)) := by
+    calc (-y) ⊖ (-z) ⊕ (- z)
+    _ = (-z) ⊖ (-y) ⊕ (-y) := by rw[not_switch']
+    _ = (-z) ⊙ (- - y) ⊕ (- y) := by rw[oNeg_def]
+    _ = - (z ⊕ - y) ⊕ -y := by rw[oTimes_dual,neg_neg,neg_neg]
+    _ = -y ⊕ - (- y ⊕ z) := by rw[oAdd_comm z,oAdd_comm]
+    _ = -(y ⊙ (-y ⊕ z)) := by rw[oTimes_dual,neg_neg]
 
 instance : Lattice A where
-  sup x y := (x ⊖ y) + y
-  inf x y := x ⊙ (- x + y)
+  sup x y := (x ⊖ y) ⊕ y
+  inf x y := x ⊙ (- x ⊕ y)
   le_sup_left := by
     intro x y
-    suffices this : x ≤  - ((-x) + y) + y from by rw[oNeg_def'] ; exact this
-    suffices this : x ≤  - ((-y) + x) + x from by rw[neg_switch] ; exact this
+    suffices this : x ≤ (y ⊖ x) ⊕ x from by rw[not_switch'] ; exact this
     apply le_iff₄.mpr
-    use (- (- y + x))
-    exact add_comm x _
+    use y ⊖ x
+    exact oAdd_comm x _
   le_sup_right := by
     intro x y
     apply le_iff₄.mpr
     use x ⊖ y
-    exact add_comm _ _
+    exact oAdd_comm _ _
   sup_le := sup_le'
   le_inf := by
     intro x y z hy hz
-    suffices this : - (y ⊙ (-y + z)) ≤ -x from by rw[neg_le'] ; exact this
-    replace hy : -y ≤ - x := (neg_le' _ _).mp hy
-    replace hz : -z ≤ - x := (neg_le' _ _).mp hz
-    suffices this : (-y) ⊖ (-z) + (- z) ≤ - x from by rw[←neg_sup'] ; exact this
+    suffices this : - (y ⊙ (-y ⊕ z)) ≤ -x from by rw[not_le'] ; exact this
+    replace hy : -y ≤ - x := (not_le' _ _).mp hy
+    replace hz : -z ≤ - x := (not_le' _ _).mp hz
+    suffices this : ((-y) ⊖ (-z) ⊕ (- z)) ≤ - x from by rw[←not_sup'] ; exact this
     exact sup_le' _ _ _ hy hz
   inf_le_left := by
     intro x y
-    suffices h : -( - x + - (-x + y)) ≤ x from by rw[oTimes_dual] ; exact h
-    suffices h : - x ≤ - x + - (-x + y) from by rw[neg_le',neg_neg] ; exact h
+    suffices h : - x ≤ - (x ⊙ (- x ⊕ y)) from by rw[not_le'] ; exact h
+    suffices h : - x ≤ (-x) ⊖ (-y) ⊕ (-y) from by rw[←not_sup'] ; exact h
+    suffices h : - x ≤ (-y) ⊖ (-x) ⊕ (-x) from by rw[not_switch'] ; exact h
     rw[le_iff₄]
-    use -(-x + y)
+    use (-y) ⊖ (-x)
+    rw[oAdd_comm]
   inf_le_right := by
     intro x y
-    suffices h : - y ≤ - (x ⊙ (-x + y)) from by rw[neg_le'] ; exact h
-    suffices h : - y ≤ (-x) ⊖ (-y) + (- y) from by rw[←neg_sup'] ; exact h
-    suffices h : - y ≤ - y + (-x) ⊖ (-y) from by rw[add_comm] ; exact h
+    suffices h : - y ≤ - (x ⊙ (-x ⊕ y)) from by rw[not_le'] ; exact h
+    suffices h : - y ≤ (-x) ⊖ (-y) ⊕ (- y) from by rw[←not_sup'] ; exact h
+    suffices h : - y ≤ - y ⊕ (-x) ⊖ (-y) from by rw[oAdd_comm] ; exact h
     rw[le_iff₄]
     use (-x)⊖(-y)
+
+@[simp]
+lemma inf_def (x y : A) : x ⊓ y = x ⊙ (- x ⊕ y) := rfl
+
+@[simp]
+lemma sup_def (x y : A) : x ⊔ y = (x ⊖ y) ⊕ y := rfl
+
+@[simp]
+lemma not_sup (x y : A) : - (x ⊔ y) = (-x) ⊓ (-y) := by
+  rw[inf_def,sup_def]
+  suffices this : - ((- - x) ⊖ (- - y) ⊕ - - y) = (-x) ⊙ (- - x ⊕ - y) from by
+    nth_rw 1 [←neg_neg x]
+    nth_rw 1 [← neg_neg y]
+    nth_rw 2 [← neg_neg y]
+    exact this
+  rw[not_sup' (-x) (-y)]
+  rw[neg_neg]
+
+@[simp]
+lemma not_inf (x y : A) : - (x ⊓ y) = (-x) ⊔ (-y) := by
+  rw[not_iff_not',neg_neg]
+  rw[not_sup,neg_neg,neg_neg]
+
+lemma oTimes_le_dual (x y z : A) : x ⊙ y ≤ z ↔ x ≤ - y ⊕ z := by
+  calc x ⊙ y ≤ z
+  _ ↔ (- (x ⊙ y) ⊕ z) = 1 := by rw[le_iff₁]
+  _ ↔ ((- x ⊕ - y) ⊕ z) = 1 := by rw[oTimes_dual']
+  _ ↔ (- x ⊕ (- y ⊕ z)) = 1 := by rw[oAdd_assoc]
+  _ ↔ x ≤ - y ⊕ z := by rw[le_iff₁]
+
+lemma oTimes_sup (x y z : A) : x ⊙ (y ⊔ z) = (x ⊙ y) ⊔ (x ⊙ z) := by
+  have h₁ : (x ⊙ y) ≤ x ⊙ (y ⊔ z) := by
+    apply le_oTimes
+    exact le_sup_left
+  have h₂ : (x ⊙ z) ≤ x ⊙ (y ⊔ z) := by
+    apply le_oTimes
+    exact le_sup_right
+  have h_le := (@sup_le_iff A _ _ _ _).mpr ⟨h₁,h₂⟩
+  have le_h : x ⊙ (y ⊔ z) ≤ (x ⊙ y) ⊔ (x ⊙ z) := by
+    let t := (x ⊙ y) ⊔ (x ⊙ z)
+    have h₁' : x ⊙ y ≤ t := by unfold t ; apply le_sup_left
+    have h₂' : x ⊙ z ≤ t := by unfold t ; apply le_sup_right
+    replace h₁' : y ≤ (- x) ⊕ t := by rw[←oTimes_le_dual,oTimes_comm] ; exact h₁'
+    replace h₂' : z ≤ (- x) ⊕ t := by rw[←oTimes_le_dual,oTimes_comm] ; exact h₂'
+    have h' : y ⊔ z ≤ (- x) ⊕ t := sup_le h₁' h₂'
+    replace h' : x ⊙ (y ⊔ z) ≤ t := by rw[oTimes_comm,oTimes_le_dual] ; exact h'
+    apply h'
+  exact le_antisymm le_h h_le
+
+lemma add_inf' (x y z : A) : (x ⊕ (y ⊓ z)) = (x ⊕ y) ⊓ (x ⊕ z) := by
+  rw[not_iff_not']
+  rw[not_inf]
+  rw[oAdd_dual']
+  rw[not_inf]
+  rw[oAdd_dual,oAdd_dual]
+  rw[neg_neg,neg_neg]
+  apply oTimes_sup
